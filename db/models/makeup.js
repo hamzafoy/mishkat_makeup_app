e65619f57bd281b46e198d52f8e15c2868bf061e
Ross Foy <hamzaoflouisville@gmail.com>
2021-05-11 16:06:53 -0400

    add db rows to the Makeup model.

--- db/models/makeup.js
+++ db/models/makeup.js
@@ -4,7 +4,19 @@ const Sequelize = require('sequelize');
 module.exports = (sequelize) => {
     class Makeup extends Sequelize.Model {}
     Makeup.init({
-        
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: Sequelize.STRING(10000),
+            allowNull: false
+        },
+        description: {
+            type: Sequelize.STRING(10000),
+            allowNull: false
+        }
     }, {sequelize});
 
     return Makeup;
