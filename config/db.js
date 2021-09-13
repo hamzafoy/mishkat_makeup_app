b9e2159657eb9ab98ee2fd51d10597d1770a9f45
Ross Foy <hamzaoflouisville@gmail.com>
2021-09-13 17:24:52 -0400

    building out MongoDB configuration.

---
+++ config/db.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = process.env.MONGODB_URI || config.get('mongoURI');
+
+
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(
+      db,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
+    );
+
+    console.log('MongoDB is Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+
+
+module.exports = connectToDatabase;
